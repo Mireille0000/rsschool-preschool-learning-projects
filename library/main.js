5fdc685383bbbcfd9a77127e0c1b8b740a94d1da
Gasanova Tamara <solas93@mail.ru>
2023-08-25 00:38:48 +0300

    feat: add drop down menu before autorization

--- library/main.js
+++ library/main.js
@@ -23,10 +23,14 @@ const burgerFunction = () => {
     const burgerMenuItem = document.querySelector('li');
     const main = document.querySelector('main');
 
+    const menuBeforeAutorization = document.querySelector('.drop-menu-no-authorization');
+
 
     burgerIcon.addEventListener('click', () => {
         burgerMenu.classList.toggle('burger-menu_active');
         burgerIcon.classList.toggle('burger-icon-active');
+
+        menuBeforeAutorization.classList.remove('drop-menu-no-authorization_active');
     })
 
     burgerIcon.onclick = function(event) {
@@ -99,32 +103,58 @@ function favorites () {
         spring.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         summer.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         autumn.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
-        setTimeout(() => (winter.style = 'position: absolute; z-index: 3; display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
+        setTimeout(() => (winter.style = 'display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
     })
 
     radioButtonsArray[1].addEventListener('click', () => {
         winter.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         summer.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         autumn.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
-        setTimeout(() => (spring.style = 'position: absolute; z-index: 3; opacity: 1; display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
+        setTimeout(() => (spring.style = 'opacity: 1; display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
     })
 
     radioButtonsArray[2].addEventListener('click', () => {
         winter.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         spring.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         autumn.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
-        setTimeout(() => (summer.style = 'position: absolute; z-index: 3; opacity: inherit; display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
+        setTimeout(() => (summer.style = 'opacity: inherit; display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
     })
 
     radioButtonsArray[3].addEventListener('click', () => {
         winter.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         spring.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
         summer.style = 'display: flex; flex-wrap: wrap; opacity: 0; transition-duration: 1s';
-        setTimeout(() => (autumn.style = 'position: absolute; z-index: 3; opacity: inherit; display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
+        setTimeout(() => (autumn.style = 'opacity: inherit; display: flex; column-gap: 259px; flex-wrap: wrap; transition-duration: 1s'), "1000");
     })       
 }
 
 favorites();
+
+// modal windows
+
+function dropMenus () {
+    const iconHeader = document.querySelector('.icon-header');
+    const menuBeforeAutorization = document.querySelector('.drop-menu-no-authorization');
+
+    const burgerIcon = document.querySelector('.burger-icon');
+    const burgerMenu = document.querySelector('.burger-menu');     
+
+    const main = document.querySelector('main');
+
+
+    iconHeader.addEventListener('click', () => {
+        console.log('Hi');
+        menuBeforeAutorization.classList.toggle('drop-menu-no-authorization_active');
+        burgerMenu.classList.remove('burger-menu_active');
+        burgerIcon.classList.remove('burger-icon-active');  
+        })
+
+        main.addEventListener('click', () => {
+            menuBeforeAutorization.classList.remove('drop-menu-no-authorization_active');
+        })
+}
+
+dropMenus ();
 
 
 console.log('50/50: бургер меню не исчезает при нажатии на header, в ТЗ - при нажатии на крестик, или на область вне меню, адаптивное меню плавно скрывается, уезжая за экран. У меня - только при нажатии на main. Возможно, стоит снять балл')
