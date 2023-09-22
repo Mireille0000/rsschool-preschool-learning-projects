2686cc6d15a21b1f2b39b97b1483126730feeb2a
Gasanova Tamara <solas93@mail.ru>
2023-09-22 15:59:30 +0300

    feat: add showCurrentTrackeTitle function changing track title

--- Audio-player/main.js
+++ Audio-player/main.js
@@ -2,5 +2,20 @@ const trackImage = document.querySelector('.tarck-image'),
     buttonPause = document.querySelector('.button-pause'),
     buttonPlay = document.querySelector('.button-play'),
     currentTrack = document.querySelector(".current"), // change so that the two lines change
+    artist = document.querySelector(".artist"),
+    track = document.querySelector(".track")
     buttonPrevious = document.querySelector(".button-prev"),
-    buttonNext = document.querySelector(".button-next")
+    buttonNext = document.querySelector(".button-next");
+
+    const tracksArtist = ["Kokia", "Eels"];
+    const tracks = ["Fukurou", "Lone Wolf"];
+
+    let artistIndex = 0;
+    let trackIndex = 0;
+
+    function showCurrentTrackeTitle(tracksArtist, tracks) {
+        artist.innerHTML = tracksArtist;
+        track.innerHTML = tracks;    
+    }
+
+    showCurrentTrackeTitle(tracksArtist[artistIndex], tracks[trackIndex]);
