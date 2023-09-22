159fd1970b9488466c76652fc67b52625ab5eb19
Gasanova Tamara <solas93@mail.ru>
2023-09-22 15:02:12 +0300

    feat: change footer styles and progress bar styles, add audio files

--- Audio-player/main.js
+++ Audio-player/main.js
@@ -1,3 +1,6 @@
-function doSomthing() {
-    
-}
+const trackImage = document.querySelector('.tarck-image'),
+    buttonPause = document.querySelector('.button-pause'),
+    buttonPlay = document.querySelector('.button-play'),
+    currentTrack = document.querySelector(".current"), // change so that the two lines change
+    buttonPrevious = document.querySelector(".button-prev"),
+    buttonNext = document.querySelector(".button-next")
