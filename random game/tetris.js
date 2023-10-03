d02240d3f9ad941c7347acf399093b3a3d6a30e3
Gasanova Tamara <solas93@mail.ru>
2023-10-03 22:27:01 +0300

    feat: initiate variables

---
+++ random game/tetris.js
@@ -0,0 +1,12 @@
+const gameField = document.querySelector(".game-field"),
+    fieldItem = document.querySelectorAll(".game-field div"),
+
+    score = document.querySelector(".score"),
+    level = document.querySelector(".level"),
+
+    nextField = document.querySelector(".next"),
+    nextItem = document.querySelector(".next div")
+
+const width = 10;
+
+console.log(`add buttons`)
